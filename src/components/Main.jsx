32054fdfb5702f037458cbc806c62959373b5ca3
Abass Olanrewaju <abassolanrewaju111@gmail.com>
2022-11-29 11:47:52 +0100

    Create function that generates array of random numbers

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,19 +2,14 @@ import React from 'react';
 import Die from './Die';
 
 export default function Main() {
+  const allNewDice = () => Array.from({ length: 10 }, () => Math.floor(Math.random() * 7) + 1);
+  console.log(allNewDice());
+
+  const dieEl = allNewDice().map((die) => <Die value={die} />);
   return (
     <main>
       <div className="dice">
-        <Die value={1} />
-        <Die value={2} />
-        <Die value={3} />
-        <Die value={4} />
-        <Die value={5} />
-        <Die value={6} />
-        <Die value={4} />
-        <Die value={5} />
-        <Die value={3} />
-        <Die value={2} />
+        {dieEl}
       </div>
     </main>
   );
